4d77538df8cc4683cba829df1facc3f1e062fead
Dawidx3p <maildawcio@gmail.com>
2021-10-19 00:43:17 +0200

    applying react-router part5.9

--- src/components/Film/Film.js
+++ src/components/Film/Film.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Film(props) {
     return(
         <div>
-        {props.film && <div>{props.film.title.title}</div>}
+        {props.film.title.title && <div>{props.film.title.title}</div>}
         </div>
     );
 }
